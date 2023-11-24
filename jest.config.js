f8118ad7e4250b8728e8baa57a680a302b1635d7
houyw <houyaowei@163.com>
2023-11-24 15:29:53 +0800

    :dizzy: first push

--- jest.config.js
+++ jest.config.js
@@ -93,7 +93,7 @@ module.exports = {
   // notifyMode: "failure-change",
 
   // A preset that is used as a base for Jest's configuration
-  // preset: undefined,
+  preset: "ts-jest",
 
   // Run tests from one or more projects
   // projects: undefined,
@@ -173,9 +173,9 @@ module.exports = {
 
   // A map from regular expressions to paths to transformers
   // transform: undefined,
-  "transform": {
-    "\\.m?jsx?$": "jest-esm-transformer"
-  },
+    "transform": {
+      "^.+\\.(t|j)sx?$": "ts-jest"
+    },
   // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
   // transformIgnorePatterns: [
   //   "/node_modules/",
--- rollup.config.js
+++
@@ -1,8 +0,0 @@
-export default {
-  input: './src/main.js',
-  output: {
-    file: 'dist/javascript-validate-utils.js',
-    format: 'esm',
-    name: 'javascript-validate-utils'
-  }
-}
